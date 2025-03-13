0d6d694212cad6597f386d8c107a6ffd07a536e3
Paul Hinterbauer <phinterbauer.privat@gmail.com>
2025-03-13 13:58:42 +0100

    created class

---
+++ main.py
@@ -0,0 +1,12 @@
+class Bisektion():
+    def __init__(self):
+        self.a:float = 0.0
+        self.b:float = 0.0
+        self.c:float = 0.0
+        self.fa:float = 0.0
+        self.fb:float = 0.0
+        self.fc:float = 0.0
+        self.accuracy:float = 0.0
+        self.funktion:str = ""
+        self.controll:bool = False
+
