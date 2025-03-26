827b732863d6628446e3deb8e03d9923fa1498d5
Paul Hinterbauer <phinterbauer.privat@gmail.com>
2025-03-26 21:53:55 +0100

    sigma

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ class Bisektion():
         self.fb:float = 0.0
         self.fc:float = 0.0
         self.accuracy:float = 0.0
-        self.formula:str = "np.sqrt(self.n) - {x}"
+        self.controll_value:float = 0.0
+        self.result:dict = {}
+        self.formula:str = ""
         self.controll:bool = False
 
     def get_n(self):
@@ -56,21 +58,82 @@ class Bisektion():
         self.fc = eval(self.formula.format(x = self.c))
     
     def check_controll(self):
-        if self.fa * self.fc < 0:
+        self.controll_value = self.fa * self.fb
+        if self.controll_value < 0:
             self.controll = True
         else:
             self.controll = False
     
     def switch_interval(self):
-        ...
+        if self.fa * self.fc < 0:
+            self.b = self.c
+        else:
+            self.a = self.c
 
     def main_loop(self):
-        ...
+        iteration = 1
+        self.result = {}
+        self.get_n()
+        self.get_interval_a()
+        self.get_interval_b()
+        self.calculate_fa()
+        self.calculate_fb()
+        self.calculate_c()
+        self.calculate_fc()
+        self.check_controll()
+        
+        print("="*28, "Iteration {iteration}".format(iteration = iteration), "="*28)
+        print(f'a: {self.a}, b: {self.b}, c: {self.c}')
+        print(f'fa: {self.fa}, fb: {self.fb}, fc: {self.fc}')
+        print(f'controll: {self.controll}, controll value: {self.controll_value}')
+        print("="*69)
+
+        self.switch_interval()
+
+        while abs(self.fc) > self.accuracy and self.controll:
+            iteration += 1
+            self.calculate_fa()
+            self.calculate_fb()
+            self.calculate_c()
+            self.calculate_fc()
+            self.check_controll()
+
+            print("="*28, "Iteration {iteration}".format(iteration = iteration), "="*28)
+            print(f'a: {self.a}, b: {self.b}, c: {self.c}')
+            print(f'fa: {self.fa}, fb: {self.fb}, fc: {self.fc}')
+            print(f'controll: {self.controll}, controll value: {self.controll_value}')
+            print("="*70)
+
+            self.switch_interval()
+
+            if self.fc == 0:
+                self.result["c:"] = self.c
+                break
+            elif self.fa == 0:
+                self.result["a:"] = self.a
+                break
+            elif self.fb == 0:
+                self.result["b:"] = self.b
+                break
+        
+        if not self.controll:
+            print("No solution found!")
+        else:
+            if self.fa * self.fc < 0:
+                self.result["b:"] = self.b
+                self.result["c:"] = self.c
+            else:
+                self.result["a:"] = self.a
+                self.result["c:"] = self.c
+
+        # fix result print
+        print("="*31, "Result", "="*31)
+        for key, value in self.result.items():
+            print(f'{key} {value}')
+        print("="*70)
 
 if __name__ == "__main__":
     bisektion = Bisektion()
-    # move to main loop
-    bisektion.get_n()
-    bisektion.get_interval_a()
-    bisektion.get_interval_b()
-    bisektion.calculate_fa()
+    bisektion.formula = "np.sqrt(self.n) - {x}"
+    bisektion.accuracy = 0.001
+    bisektion.main_loop()
